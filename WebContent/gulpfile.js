36b6774fb837db4a8c76f1aaf7aa9956d7cf9383
StoneSoftware <wu_firefox@163.com>
2020-02-08 17:37:45 +0800

    compress

--- WebContent/gulpfile.js
+++ WebContent/gulpfile.js
@@ -4,6 +4,7 @@ var jshint = require("gulp-jshint");//js语法检查
 var filter = require("gulp-filter");//过滤
 var runSequence = require("run-sequence");//按顺序执行task
 var uglify = require('gulp-uglify');//js压缩
+var minifyCss = require('gulp-clean-css');
 
 
 var AppConfig = {
@@ -14,7 +15,7 @@ var AppConfig = {
 
 
 
-gulp.task("server", function () {
+gulp.task("server", function() {
     connect.server({
         root: AppConfig.ROOT,
         livereload: true,
@@ -22,13 +23,28 @@ gulp.task("server", function () {
     });
 });
 
-gulp.task("check", function () {
-    var f = filter(["**", "!*app/bower_components/**", "!*app/isteven-multi-select/script/res/**"]);
+gulp.task("check", function() {
+    var f = filter(["**", "!*app/bower_components/**", "!*app/isteven-multi-select/scripts/res/**"]);
     return gulp.src("app/**/*.js")
         .pipe(f)
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         .pipe(jshint.reporter('fail'));
 });
-gulp.task("server:dev", gulp.series("check", "server"));
+
+gulp.task("compressJS", function() {
+    gulp.src(['app/**/scripts/*.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task("compressCSS", function() {
+    gulp.src(['app/**/css/*.css'])
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist'));
+});
+
+var compress = gulp.parallel("compressJS", "compressCSS");
+
+gulp.task("server:dev", gulp.series("check", compress, "server"));
 
