5f6d1464654d40c63700ca08dce45d37dc75e7b6
StoneSoftware <wu_firefox@163.com>
2020-02-08 20:59:10 +0800

    code

--- WebContent/gulpfile.js
+++ WebContent/gulpfile.js
@@ -4,7 +4,9 @@ var jshint = require("gulp-jshint");//js语法检查
 var filter = require("gulp-filter");//过滤
 var runSequence = require("run-sequence");//按顺序执行task
 var uglify = require('gulp-uglify');//js压缩
-var minifyCss = require('gulp-clean-css');
+var minifyCss = require('gulp-clean-css');//css压缩
+var htmlmin = require('gulp-htmlmin');//html压缩
+var del = require('del');
 
 
 var AppConfig = {
@@ -15,7 +17,7 @@ var AppConfig = {
 
 
 
-gulp.task("server", function() {
+gulp.task("server", function () {
     connect.server({
         root: AppConfig.ROOT,
         livereload: true,
@@ -23,7 +25,7 @@ gulp.task("server", function() {
     });
 });
 
-gulp.task("check", function() {
+gulp.task("check", function () {
     var f = filter(["**", "!*app/bower_components/**", "!*app/isteven-multi-select/scripts/res/**"]);
     return gulp.src("app/**/*.js")
         .pipe(f)
@@ -32,19 +34,38 @@ gulp.task("check", function() {
         .pipe(jshint.reporter('fail'));
 });
 
-gulp.task("compressJS", function() {
+gulp.task("compressJS", function () {
     gulp.src(['app/**/scripts/*.js'])
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task("compressCSS", function() {
+gulp.task("compressCSS", function () {
     gulp.src(['app/**/css/*.css'])
         .pipe(minifyCss())
         .pipe(gulp.dest('dist'));
 });
 
-var compress = gulp.parallel("compressJS", "compressCSS");
+gulp.task('compressHTML', function () {
+    var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: false,//压缩页面JS
+        minifyCSS: false//压缩页面CSS
+    };
+    gulp.src('app/**/html/*.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('dist'));
+});
+gulp.task('clean', function (cb) {
+    del(['dist/**/*'], cb);
+});
 
-gulp.task("server:dev", gulp.series("check", compress, "server"));
+var compress = gulp.parallel("compressJS", "compressCSS", "compressHTML");
+
+gulp.task("server:dev", gulp.series("clean", "check", compress, "server"));
 
